84328518d4454f2d629ee5ff2bf9cffcd2d5d3ed
Noam Amrani <noama@sela.co.il>
2022-08-26 19:03:39 +0300

    install helm

--- install-kube-tools.sh
+++ install-kube-tools.sh
@@ -26,3 +26,10 @@ echo 'source <(kubectl completion bash)' >>/home/vagrant/.bashrc
 echo 'alias k=kubectl' >>/home/vagrant/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>/home/vagrant/.bashrc
 source /home/vagrant/.bashrc
+
+# install helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
